b6b7649f899bc8035113cbb07159d839fa9feb2e
Kai Giese <kaigieseail.de>
2020-03-20 20:28:27 +0100

    html div closed image renamed

--- Großproject/gotech.js
+++ Großproject/gotech.js
@@ -20,40 +20,28 @@ closeButton.onclick = menuButtonClickhandler;
 menuButton.onclick = menuButtonClickhandler;
 
 const form = document.getElementsByTagName("form")[0];
-
 const email = document.getElementById("email");
 const emailError = document.querySelector("#email + span.error");
 
 email.addEventListener("input", function() {
-  // Each time the user types something, we check if the
-  // form fields are valid.
-
   if (email.validity.valid) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
-    emailError.innerHTML = ""; // Reset the content of the message
-    emailError.className = "error"; // Reset the visual state of the message
+    emailError.innerHTML = "";
+    emailError.className = "error";
   } else {
-    // If there is still an error, show the correct error
     showError();
   }
 });
 
 form.addEventListener("submit", function(event) {
-  // if the email field is valid, we let the form submit
-
   if (!email.validity.valid) {
-    // If it isn't, we display an appropriate error message
     showError();
-    // Then we prevent the form from being sent by canceling the event
+
     event.preventDefault();
   }
 });
 
 function showError() {
   if (email.validity.valueMissing) {
-    // If the field is empty
-    // display the following error message.
     emailError.textContent = "You need to enter an e-mail address.";
   } else if (email.validity.typeMismatch) {
     emailError.textContent = "Entered value needs to be an e-mail address.";
@@ -77,4 +65,21 @@ function submitForm(event) {
   );
 }
 
-submitButton.addEventListener("click", submitForm);
+/*submitButton.addEventListener("click", submitForm);*/
+
+submitButton.addEventListener("click", () => {
+  if (
+    name.value.length > 0 &&
+    firstName.value.length > 0 &&
+    email.value.length > 3 &&
+    email.value.includes("@") &&
+    message.value.length > 0
+  ) {
+    openModal(event);
+  } else {
+    customerInputs.forEach(btn => btn.classList.add("error"));
+    alert(
+      "Please make sure that you filled in all the required fields. Thank you!"
+    );
+  }
+});
